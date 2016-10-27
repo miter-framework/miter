0132e20e9c9d146b83763a06fc1256d424bb6521
Brandon <brandonyoyoslade@gmail.com>
2016-10-27 16:27:13 -0600

    Improved type of RouteFunc; no callback parameter

--- src/router/route-func.ts
+++ src/router/route-func.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 
-export type VoidT = void | Promise<void>;
-export type RouteFunc = (req: express.Request, res: express.Response, next?: express.NextFunction) => VoidT;
+export type RouteFunc = (req: express.Request, res: express.Response) => void | Promise<void>;
 export type RouteFuncDescriptor = TypedPropertyDescriptor<RouteFunc>;
 export type RouteDecoratorFunc = (target: Object, propertyKey: string, propertyDescriptor: RouteFuncDescriptor) => void;
