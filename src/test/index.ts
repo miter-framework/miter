462cea92c1bba9d7c2c1dcf32d0908b1e2113530
Brandon <brandonyoyoslade@gmail.com>
2016-11-19 17:45:34 -0700

    Add test folder and launch config to help test miter without api-server

---
+++ src/test/index.ts
@@ -0,0 +1,16 @@
+import { cin } from './util/cin';
+import { SnakeCaseOrmTransformService } from '../services/snake-case-orm-transform.service';
+
+(async function() {
+   console.log('Hello, World!');
+   let transformSvc = new SnakeCaseOrmTransformService();
+   
+   while (true) {
+      let line = await cin.readline();
+      if (line == 'exit') break;
+      console.log('Model Name:', transformSvc.transformModelName(line, {}));
+      console.log('Column Name:', transformSvc.transformColumnName(line, {}));
+   }
+   
+   process.exit(0);
+})();
