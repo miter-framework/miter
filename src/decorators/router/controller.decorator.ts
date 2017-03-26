5e167e98d324c68928d1e2dd007eb9d421cfc849
Brandon <brandonyoyoslade@gmail.com>
2017-03-25 21:04:37 -0600

    fix(controller.decorator): use the passed in string as a default path

--- src/decorators/router/controller.decorator.ts
+++ src/decorators/router/controller.decorator.ts
@@ -2,10 +2,10 @@ import { CtorT } from '../../core/ctor';
 import { ControllerT } from '../../core/controller';
 import { ControllerMetadata, ControllerMetadataSym, ControllerRoutesSym } from '../../metadata';
 
-export function Controller(meta?: ControllerMetadata) {
-    meta = meta || {};
+export function Controller(meta?: ControllerMetadata | string) {
+    let controllerMeta = typeof meta === 'string'? { path: meta } : (meta || {});
     return function(controller: CtorT<ControllerT>) {
-        Reflect.defineMetadata(ControllerMetadataSym, meta, controller.prototype);
+        Reflect.defineMetadata(ControllerMetadataSym, controllerMeta, controller.prototype);
         
         let routes: string[] = Reflect.getOwnMetadata(ControllerRoutesSym, controller.prototype) || [];
         Reflect.defineMetadata(ControllerRoutesSym, routes, controller.prototype);
