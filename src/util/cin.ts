705fadc477ef638d70326b837e2b347688718770
Brandon <brandonyoyoslade@gmail.com>
2017-04-13 23:04:30 -0600

    fix(cin): create a console input interface only when requested

--- src/util/cin.ts
+++ src/util/cin.ts
@@ -2,32 +2,38 @@ import { createInterface, ReadLine } from 'readline';
 
 export interface ReadLineAsync {
     readline(): Promise<string>,
-    native: ReadLine,
+    getNative: () => ReadLine,
     pause: () => void,
     resume: () => void,
     emit: (line: string) => void
 }
 
-const nativeCin = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+let nativeCin: ReadLine | null = null;
+function getNativeCin() {
+    if (!nativeCin) {
+        nativeCin = createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        nativeCin.on('line', (line: string) => emitLine(line));
+    }
+    return nativeCin;
+}
 let resolveFns: { (str: string): void }[] = [];
 function emitLine(line: string) {
     if (!resolveFns.length) return;
     let resolveFn = resolveFns.splice(0, 1)[0];
     resolveFn(line.trim());
 }
-nativeCin.on('line', (line: string) => emitLine(line));
 export const cin: ReadLineAsync = {
     readline: () => {
         return new Promise((resolve, reject) => {
             resolveFns.push(resolve);
-            nativeCin.prompt();
+            getNativeCin().prompt();
         });
     },
-    native: nativeCin,
-    pause: () => void nativeCin.pause(),
-    resume: () => void nativeCin.resume(),
+    getNative: getNativeCin,
+    pause: () => void getNativeCin().pause(),
+    resume: () => void getNativeCin().resume(),
     emit: (line: string) => emitLine(line)
 };
