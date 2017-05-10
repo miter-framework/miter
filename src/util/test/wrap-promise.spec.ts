cc2d6dccab767960a40fa24606c8852d8fec5e73
Brandon <brandonyoyoslade@gmail.com>
2017-05-10 13:57:08 -0600

    test(util): add unit tests for wrapPromise

---
+++ src/util/test/wrap-promise.spec.ts
@@ -0,0 +1,17 @@
+/// <reference types="mocha" />
+
+import { expect, use } from 'chai';
+import * as sinon from 'sinon';
+import * as sinonChai from 'sinon-chai';
+use(sinonChai);
+
+import { wrapPromise } from '../wrap-promise';
+
+describe('util/wrapPromise', () => {
+    it('should resolve the promise when the callback function is invoked', async () => {
+        let callbackFn = (millis: number, done: Function) => {
+            setTimeout(done, millis);
+        }
+        await wrapPromise(callbackFn, 10);
+    });
+});
