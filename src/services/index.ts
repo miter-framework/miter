4466cc43ac3bd247dda6f6b0756b7bdcfe4b4b66
Brandon <brandonyoyoslade@gmail.com>
2017-06-24 21:11:37 -0600

    fix(services): ensure LoggerCore is exported along with Logger

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,7 @@
 export * from './reflector';
 
 export * from './logger';
+export * from './logger-core';
 
 export * from './orm-transform.service';
 export * from './snake-case-orm-transform.service';
