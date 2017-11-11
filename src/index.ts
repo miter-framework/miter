70c4475f17d1bf6c0f7696247e6629f4d2873909
Brandon <brandonyoyoslade@gmail.com>
2017-11-11 00:19:33 -0700

    chore(*): remove OrmReflector; functionality extracted to miter-sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+
+
 export * from './controllers';
 export * from './core';
 export * from './decorators';
 export * from './metadata';
-export * from './orm';
 export * from './policies';
 export * from './services';
 export * from './router';
